90f1fe9236af64e83f956150f6d4a4694d1511a5
Sawada Tsunayoshi <animekaizokux@gmail.com>
2020-09-19 08:56:34 +0530

    Pushing requirements and stablize venv

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @echo off
-TITLE Saitama Robot
-:: Enables virtual env mode and then starts saitama
-env\scripts\activate.bat && py -m SaitamaRobot
+TITLE ShellderRobot
+:: Enables virtual env mode and then starts ShellderRobot
+env\scripts\activate.bat && py ShellderRobot
